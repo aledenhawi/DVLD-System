aa9186341629dd1b1191de0e7e885c518a03100c
Ali Denhawi <aledenhawez216@gmail.com>
2026-07-02 11:02:19 +0300

    I fixed the code

--- DVDL-DataLayer/clsDataAccessSettings.cs
+++ DVDL-DataLayer/clsDataAccessSettings.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace DVDL_DataLayer
 {
     internal class clsDataAccessSettings
     {
-        public static string ConnectionString = "Server=.;Database=DVLD_DB;Integrated Security=True;TrustServerCertificate=True;";
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
     }
 }
